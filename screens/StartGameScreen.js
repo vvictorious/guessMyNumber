d8498c3d1d820d127a76a86cc247bf5b47579b0f
vvictorious <vvictorious30@gmail.com>
2024-01-01 12:06:15 -0800

    styling the custom buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,12 +1,32 @@
-import { TextInput, View } from 'react-native'
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = () => {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+};
 
-    return (
-      <View>
-				<TextInput />
-			</View>
-    )
-}
+export default StartGameScreen;
 
-export default StartGameScreen
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+		shadowRadius: 6,
+		shadowOpacity: 0.25
+  },
+});
