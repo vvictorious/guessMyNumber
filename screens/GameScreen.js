5200b9a3fb155857d3056928f19a3bc63107803f
vvictorious <vvictorious30@gmail.com>
2024-01-04 14:12:45 -0800

    added the guess random number function to the ui

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Colors from "../constants/colors";
-const GameScreen = () => {
+
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function generateRandomBetween(min, max, exclude) {
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+    if (rndNum === exclude) {
+      return generateRandomBetween(min, max, exclude);
+    } else {
+      return rndNum;
+    }
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Opponenets Guess</Text>
+      <View style={styles.numberContainer}>
+        <Text style={styles.numberText}>{currentGuess}</Text>
+      </View>
       <View>
         <Text>Higher or Lower?</Text>
       </View>
@@ -21,13 +40,27 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
   },
-	title: {
-		fontSize: 24,
-		fontWeight: 'bold',
-		color: Colors.accent500,
-		textAlign: 'center',
-		borderWidth: 2,
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: 'white',
+    textAlign: "center",
+    borderWidth: 2,
+    borderColor: 'white',
+    padding: 12,
+  },
+	numberContainer: {
+		borderWidth: 4,
 		borderColor: Colors.accent500,
-		padding: 12
+		padding: 24,
+		borderRadius: 8,
+		margin: 24,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	numberText: {
+		color: Colors.accent500,
+		fontSize: 36,
+		fontWeight: 'bold'
 	}
 });
